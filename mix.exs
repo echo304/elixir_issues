28719c31522e4606fb94f33e3e1fb1a51ba94c74
Sangboak Lee <echo3042@gmail.com>
2017-02-23 00:03:05 +0900

    update

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:gettext, :logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,10 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      { :httpoison, "~> 0.9" },
+      { :poison, "~> 2.2" },
+      { :gettext, "~> 0.13" }
+    ]
   end
 end
